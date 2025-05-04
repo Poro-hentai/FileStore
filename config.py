a5493f653116486033eb688327d97e00bd134501
Poro-hentai <shadowsama128@gmail.com>
2025-05-04 15:29:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,8 +19,8 @@ from logging.handlers import RotatingFileHandler
 #--------------------------------------------
 #Bot token @Botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8164290649:AAHfcvHoGdHv53wTO5-tgKsRoQeruuzsXHo")
-APP_ID = int(os.environ.get("APP_ID", "")) #Your API ID from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "") #Your API Hash from my.telegram.org
+APP_ID = int(os.environ.get("APP_ID", "21255401")) #Your API ID from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "5afa4e413bba25aadc6cdcd857804a84") #Your API Hash from my.telegram.org
 #--------------------------------------------
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002170811388")) #Your db channel Id
